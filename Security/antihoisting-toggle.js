7855edc2a83b7df04097031b9447b478c9cbf3c0
ZumoDev <72050616+ZumoDev@users.noreply.github.com>
2021-08-12 13:51:55 -0500

    Delete Security directory

--- Security/antihoisting-toggle.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'antihoisting',
-    code: `$customEmoji[check_yes;846833629627416626] Sucessfully enabled the \`Anti Hoisting\` protection.
-$setServerVar[AntiHoisting;$messaged]
-
-$onlyIf[$checkContains[$message;enable;disable]==true;$customEmoji[check_no;846833629627416626] Invalid Arg. Argument must be \`enable\` or \`disable\`.]
-$argsCheck[>1;$customEmoji[check_no;846833629627416626] Invalid Usage. (\`$getServerVar[Prefix]antihoisting <enable/disable>\`)]`
-}
