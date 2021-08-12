dfb3cf1e25a56ace2ad5b387580aff94d6300214
ZumoDev <72050616+ZumoDev@users.noreply.github.com>
2021-08-12 13:52:17 -0500

    Delete Utility directory

--- Utility/<@.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({
-  name: "$alwaysExecute",
-  code: `
-<@$mentioned[1]> is AFK:
-\`$getUserVar[AFKMessage;$mentioned[1]]\`
-$onlyIf[$getUserVar[AFK;$mentioned[1]]==true;]
-$onlyIf[$mentioned[1]!=;]
-`
-});
