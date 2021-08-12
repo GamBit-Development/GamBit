b973330e71d80340db9432ddf1b03e1d76bf7ee5
ZumoDev <72050616+ZumoDev@users.noreply.github.com>
2021-08-12 13:51:49 -0500

    Delete RPG directory

--- RPG/Premium/heal.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({
-    name: "heal",
-    code: `You have been healed!
-$setGlobalUserVar[Health;100]
-$cooldown[15m;You must wait **%time%** to heal again!]
-$onlyIf[$getGlobalUserVar[VIP]==true;You must have **VIP** Rank from the \`$getServerVar[Prefix]shop\`!]`
-})
--- RPG/health.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({
-    name: "health",
-    aliases: ['hp'],
-    code: `Your current health is :health: **$getGlobalUserVar[Heatlh]**
-You can heal by using HP Bottles. They can be found on the \`$getServerVar[Prefix]shop\`!
-`
-})
