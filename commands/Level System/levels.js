a0916233ca3e09cdbbe9149bbdeee2cdd66b6000
ZumoDev <72050616+ZumoDev@users.noreply.github.com>
2021-08-20 15:47:34 -0500

    Update levels.js

--- commands/Level System/levels.js
+++ commands/Level System/levels.js
@@ -1,10 +1,10 @@
 module.exports = [{
-    name: '',
+    name: '$alwaysExecute',
     code: `$setUserVar[XP;$sum[$getUserVar[XP];$random[$getServerVar[MinXPPerMsg];$getServerVar[MaxXPPerMsg]]]]
 $cooldown[$getServerVar[XPCooldown];]
 $onlyIf[$getServerVar[LevelEnabled]==true;]`
 }, {
-    name: '',
+    name: '$alwaysExecute',
     code: `$eval[$getServerVar[LevelUpMessage]]
 $setUserVar[Level;$sum[$getUserVar[Level];1]]
 $setUserVar[Req;$math[$getUserVar[Req]*$getServerVar[ReqMultiplier]]]
